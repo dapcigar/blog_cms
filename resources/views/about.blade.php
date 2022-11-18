5ed3456e7353ef1381b92492eae0711d4cb7079a
User <dapcigar@gmail.com>
2022-11-18 23:20:43 +0000

    Fresh upload

---
+++ resources/views/about.blade.php
@@ -0,0 +1,12 @@
+<!Doctype html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>about us </title>
+</head>
+<body>
+<h1> This is about page </h1>
+<p>
+<a href="{{route('contact.page')}}">contact</a>
+</body>
+</html>
---
+++ resources/views/contact.blade.php
@@ -0,0 +1,12 @@
+<!Doctype html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Contact </title>
+</head>
+<body>
+<h2> This is contact page </h2>
+<p>
+    <a href="{{route('about.page')}}">About</a>
+</body>
+</html>
